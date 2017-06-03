4fd2b19f55361095e46fffcbc827a53d7c5cf8c7
mishasaggi <monisha.saggi@gmail.com>
2017-06-03 18:19:07 -0500

    Add route for loading static file.

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,9 @@
 const Ops = require('./controllers/ops');
+const DataLoad = require('./controllers/data-load.js');
 
 exports.endpoints = [
     { method: 'GET',   path: '/ops/ping', config: Ops.ping },
-    { method: 'GET',   path: '/ops/status', config: Ops.status }
+    { method: 'GET',   path: '/ops/status', config: Ops.status },
+    { method: 'GET',   path: '/load/static', config: DataLoad.staticFile },
+
 ];
