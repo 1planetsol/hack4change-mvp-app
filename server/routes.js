1fe96d3989764ebc33b36d43c781a956c2640b8a
mishasaggi <monisha.saggi@gmail.com>
2017-06-04 14:03:31 -0500

    Add comments, update Readme

--- server/routes.js
+++ server/routes.js
@@ -4,5 +4,16 @@ const DataLoad = require('./controllers/data-load.js');
 exports.endpoints = [
     { method: 'GET',   path: '/ops/ping', config: Ops.ping },
     { method: 'GET',   path: '/ops/status', config: Ops.status },
-    { method: 'GET',   path: '/load/static', config: DataLoad.staticFile }
+    { method: 'GET',   path: '/load/static', config: DataLoad.staticFile },
+    { method: 'GET',   path: '/search/findAllEnergyGenerated', config: Search.findAllEnergyGenerated },
+    { method: 'GET',   path: '/search/findAllPowerGenerated', config: Search.findAllPowerGenerated },
+    { method: 'GET',   path: '/search/findAllGridEnergy', config: Search.findAllGridEnergy },
+    { method: 'GET',   path: '/search/findAllEnergyUsed', config: Search.findAllEnergyUsed },
+    { method: 'GET',   path: '/search/findAllPowerUsed', config: Search.findAllPowerUsed },
+    { method: 'GET',   path: '/search/findAllGridPower', config: Search.findAllGridPower }
+    //Other interesting stats //pending
+    // { method: 'GET',   path: '/stats/treesPlanted', config: Search.treesPlanted },
+    // { method: 'GET',   path: '/stats/milesDriven', config: Search.milesDriven },
+    // { method: 'GET',   path: '/stats/co2offset', config: Search.milesDriven }
+
 ];
---
+++ server/services/aggregations.js
@@ -0,0 +1,58 @@
+var Influx = require('influx');
+
+var client = Influx({
+    host : "localhost",
+    port : 8086,
+    username : '',
+    password : '',
+    database : 'sol_time_series_stats'
+});
+
+exports.findAllEnergyGenerated = function(cb){
+  client.query('SELECT * FROM "energy-generated";', function(err, res){
+    console.log('response/error is ', res, err);
+    cb(err, res);
+  });
+};
+
+exports.findAllPowerGenerated = function(cb){
+  client.query('SELECT * FROM "power-generated";', function(err, res){
+    // console.log('response/error is ', res, err);
+    cb(err, res);
+  });
+};
+
+exports.findAllGridEnergy = function(cb){
+  client.query('SELECT * FROM "grid-energy";', function(err, res){
+    // console.log('response/error is ', res, err);
+    cb(err, res);
+  });
+};
+
+exports.findAllEnergyUsed = function(cb){
+  client.query('SELECT * FROM "energy-used";', function(err, res){
+    // console.log('response/error is ', res, err);
+    cb(err, res);
+  });
+};
+
+exports.findAllPowerUsed = function(cb){
+  client.query('SELECT * FROM "power-used";', function(err, res){
+    // console.log('response/error is ', res, err);
+    cb(err, res);
+  });
+};
+
+exports.findAllGridPower = function(cb){
+  client.query('SELECT * FROM "grid-power";', function(err, res){
+    // console.log('response/error is ', res, err);
+    cb(err, res);
+  });
+};
+
+// Query Example
+// client.query('SELECT * FROM "grid-power" LIMIT 100;', function(err, res){
+//     // console.log('response/error is ', res, err);
+//     cb(err, res);
+// });
+
