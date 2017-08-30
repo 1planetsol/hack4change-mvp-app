c20e364918a355ae5d87c285186ae7ed834af39e
mishasaggi <monisha.saggi@gmail.com>
2017-08-30 14:37:44 -0500

    Update parsing and load to take filenames. Add querying of last 24 hours of data by day or hour. Misc updates.

--- server/services/stats.js
+++ server/services/stats.js
@@ -20,13 +20,30 @@ var Influx = require('influx');
 //     ]
 // });
 
-var client = Influx({
-    host : "localhost",
-    port : 8086,
-    username : '',
-    password : '',
-    database : 'sol_time_series_stats'
-});
+// var client = Influx({
+//     host : "localhost",
+//     port : 8086,
+//     username : '',
+//     password : '',
+//     database : 'sol_time_series_stats'
+// });
+
+var influx = new Influx.InfluxDB({
+ host: 'localhost',
+ port: 8086,
+ database: 'sol_time_series_stats',
+ schema: [
+   {
+     measurement: 'energy-generated',
+     fields: {
+       value: Influx.FieldType.FLOAT
+     },
+     tags: [
+       'userId'
+     ]
+   }
+ ]
+})
 
 //curl -i -XPOST 'http://localhost:8086/write?db=mydb' --data-binary 
 //measurement,tagkey=tagval,tagkey=tagval fieldkey=fieldval timestamp
@@ -35,8 +52,8 @@ exports.writePoint = function(series, values, tags, timestamp) {
 //CHECK series vs measurement. Checkout queries on graphana
     console.log('Writing stats to InfluxDB');
     console.log(' - series: '+series);
-    console.log(' - values: '+JSON.stringify(values));
-    console.log(' - values: '+JSON.stringify(tags));
+    console.log(' - values: '+values);
+    console.log(' - tags: '+JSON.stringify(tags));
     console.log(' - timestamp: '+timestamp); //Convert file's timestamp field to an ISO format
     // console.log(' - tags  : '+JSON.stringify(tags));
 
@@ -44,5 +61,20 @@ exports.writePoint = function(series, values, tags, timestamp) {
         err && console.error('InfluxDB: '+err);
     });
 };
+
+exports.searchMeasurements = function(queryString, callback){
+    console.log('query is ', queryString, influx);
+
+    // influx.getDatabaseNames()
+    //   .then(function(names){
+    //     console.log('databases found ', names);
+    //   })
+    
+    console.log('.......query running........');
+    influx.queryRaw('select * from "energy-generated" ').then(rawData => {
+      console.log(rawData.results[0])
+      callback(rawData)
+    })
+}
 
 //TODO run influx server, connect to graphana for testing series/ measurements
